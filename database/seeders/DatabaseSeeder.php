8ee96cd1f851d12280f9a2c55bd7dd93bcedba9f
AhmadAlharbi <ahmaadzaid7@gmail.com>
2023-11-27 19:55:22 +0300

    add relationships

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,7 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Post::factory(10)->create();
+        User::factory()
+            ->count(10)
+            ->has(Post::factory()->count(3))
+            ->create();
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([

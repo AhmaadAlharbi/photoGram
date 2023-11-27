b4706802d82928861d28406378e7e2d013730e2b
AhmadAlharbi <ahmaadzaid7@gmail.com>
2023-11-27 19:36:58 +0300

    add factory

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'description' => fake()->sentence(),
+            'slug' => fake()->regexify('[A-Za-z0-9]{10}'),
+            'user_id' => User::factory(),
+            'image' => fake()->image(null, 360, 360, 'animals', true, true, 'cats', true)
+
+        ];
+    }
+}
